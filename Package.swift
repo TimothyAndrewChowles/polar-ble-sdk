51a394bf824a4e181b4a6df54c4a7c1b02498393
Timothy Chowles <timothy.chowles@outlook.com>
2025-11-12 21:27:04 -0600

    Declare Zip dependency

--- Package.swift
+++ Package.swift
@@ -7,19 +7,31 @@ let package = Package(
         .iOS(.v14), .watchOS(.v5)
     ],
     products: [
-        .library( name: "PolarBleSdk", targets: ["PolarBleSdk"]),
+        .library(name: "PolarBleSdk", targets: ["PolarBleSdk"]),
     ],
     dependencies: [
         .package(name: "RxSwift", url: "https://github.com/ReactiveX/RxSwift.git", .exact("6.5.0")),
         .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", from: "1.6.0"),
-	.package(url: "https://github.com/marmelroy/Zip.git", from: "2.1.2"),
+        .package(url: "https://github.com/marmelroy/Zip.git", from: "2.1.2"),
     ],
     targets: [
         .target(
             name: "PolarBleSdk",
-            dependencies: ["SwiftProtobuf", "RxSwift", "Zip"],
-            path: "sources/iOS/ios-communications/Sources",
-            exclude: ["iOSCommunications/Info.plist", "PolarBleSdk/Info.plist"]
-        ),
+            dependencies: [
+                "SwiftProtobuf",
+                .product(name: "RxSwift", package: "RxSwift"),
+                .product(name: "RxCocoa", package: "RxSwift"),
+                .product(name: "Zip", package: "Zip")
+            ],
+            path: "sources",
+            exclude: [],
+            resources: [
+                .copy("technical_documentation/PolarBleSdkTechnicalDocument.pdf")
+            ],
+            swiftSettings: [
+                .define("POLAR_SDK"),
+                .define("HAS_HTTP_SERVER")
+            ]
+        )
     ]
 )
